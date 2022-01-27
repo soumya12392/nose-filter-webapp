6e1225feb36f21324cfb3e5f07ef9dc916e3a740
Soumya Singh <83289414+soumya12392@users.noreply.github.com>
2022-01-27 18:29:12 +0530

    Add files via upload

--- code.js
+++ code.js
@@ -1,3 +1,6 @@
+noseX = 0;
+noseY = 0;
+
 function preload() {
 
 }
@@ -15,6 +18,9 @@ function setup() {
 
 function draw() {
     image(video, 0, 0, 300, 300);
+    fill(255, 0, 0);
+    stroke(255, 0, 0);
+    circle(noseX, noseY, 20);
 }
 
 function take_snapshot() {
@@ -28,7 +34,9 @@ function modelLoaded() {
 function gotPoses(results) {
     if (results.length > 0) {
         console.log(results);
-        console.log("nose x = " + results[0].pose.nose.x);
-        console.log("nose y = " + results[0].pose.nose.y);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("nose x = " + noseX);
+        console.log("nose y = " + noseY);
     }
 }
