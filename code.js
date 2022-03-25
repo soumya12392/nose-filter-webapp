620bf7af1399d5c9d190e028fd513b3e89e5405a
Soumya Singh <83289414+soumya12392@users.noreply.github.com>
2022-03-25 11:57:39 +0530

    Add files via upload
    Updated code.js

--- code.js
+++ code.js
@@ -36,8 +36,8 @@ function modelLoaded() {
 function gotPoses(results) {
     if (results.length > 0) {
         console.log(results);
-        noseX = results[0].pose.nose.x;
-        noseY = results[0].pose.nose.y;
+        noseX = results[0].pose.nose.x - 10;
+        noseY = results[0].pose.nose.y - 10;
         console.log("nose x = " + noseX);
         console.log("nose y = " + noseY);
     }
